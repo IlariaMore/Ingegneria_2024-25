c374685de7fa9a14524cec8e9c28c59dd0c41900
IlariaMore <ilaria.morettini@studenti.unicam.it>
2025-04-10 17:35:46 +0200

    Update IBuyer.java

--- Marchet/api/main/java/unicam/marchet/IBuyer.java
+++ Marchet/api/main/java/unicam/marchet/IBuyer.java
@@ -1,4 +1,5 @@
-puplic interface IBuyer{
+puplic interface IBuyer implements IUser{
   void selectProduct(); //mette nel carrello
   void buy(); //compra direttamente
+  boolean joinEvent(Event event); //se l'evento non è al completo, l'acquirente si iscrive all'evento 
 }
