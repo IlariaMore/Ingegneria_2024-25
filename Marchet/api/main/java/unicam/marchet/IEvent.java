5850c56456559eb4be7d86bcfbf52eb6109c6558
IlariaMore <ilaria.morettini@studenti.unicam.it>
2025-04-10 17:49:37 +0200

    Update IEvent.java

--- Marchet/api/main/java/unicam/marchet/IEvent.java
+++ Marchet/api/main/java/unicam/marchet/IEvent.java
@@ -1,3 +1,10 @@
 public interface IEvent{ //forse da suddividere tra event e fair
+  private String name;
+  private int maxAttendees;
+  private DATE date;
+  private String description;
+
+  public int getMaxAttendees();
+  
   
 }
