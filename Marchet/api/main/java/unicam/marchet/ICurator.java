09de5e6f1eff1a238756db035b4a2c3f7d831dca
Luca Barchiesi <luca.barchiesi@studenti.unicam.it>
2025-04-10 18:01:40 +0200

    Create ICurator.java

---
+++ Marchet/api/main/java/unicam/marchet/ICurator.java
@@ -0,0 +1,9 @@
+/**
+*
+* This interface is used by the class that has the role of curator.
+*
+* @author Ilaria Morettini, Erika Aguiari, Luca Barchiesi
+*/
+public interface ICurator implements IUser {
+  
+}
