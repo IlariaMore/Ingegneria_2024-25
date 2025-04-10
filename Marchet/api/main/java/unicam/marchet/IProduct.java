a6a940fa5e7fb2550840201e4caced3469ce6081
Luca Barchiesi <luca.barchiesi@studenti.unicam.it>
2025-04-10 18:53:35 +0200

    Update IProduct.java

--- Marchet/api/main/java/unicam/marchet/IProduct.java
+++ Marchet/api/main/java/unicam/marchet/IProduct.java
@@ -1,4 +1,8 @@
 public interface IProduct {
-  void setLabel(),
-  void getLabel(),
+  void setLabel();
+  void getLabel(); 
+  void addInformation(String information);
+  void removeInformation();
+  void addCertification(Certification certification);
+  void removeCertification (Certification certification);
 }
