29ff79888394f7245baa2dbcbb29a6ae60afacac
Luca Barchiesi <luca.barchiesi@studenti.unicam.it>
2025-04-10 18:04:47 +0200

    Update ICurator.java

--- Marchet/api/main/java/unicam/marchet/ICurator.java
+++ Marchet/api/main/java/unicam/marchet/ICurator.java
@@ -1,9 +1,9 @@
 /**
 *
-* This interface is used by the class that has the role of curator.
+* This interface is used by the class checks the informations added by the users in the marketplace.
 *
 * @author Ilaria Morettini, Erika Aguiari, Luca Barchiesi
 */
 public interface ICurator implements IUser {
-  
+   
 }
