801acdfbe6bf4186992b547916b8dd4a493ef13f
erik-agu <erika.aguiari@studenti.unicam.it>
2025-04-10 18:07:19 +0200

    Update Market.java

--- Marchet/api/main/java/unicam/marchet/Market.java
+++ Marchet/api/main/java/unicam/marchet/Market.java
@@ -1,7 +1,9 @@
 public interface Market {
   private Database database;
 
+  // richiamato dall'acquirente 
   public void showProducts();
+  // richiamati dal venditore
   public void deleteproduct(Vendor v, Product p);
   public void addProduct(Vendor v, Product p)
 }
