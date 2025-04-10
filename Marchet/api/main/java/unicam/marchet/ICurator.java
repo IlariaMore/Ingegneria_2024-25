1ac33a9a28a24cc3dfb2037d4522460d888de019
Luca Barchiesi <luca.barchiesi@studenti.unicam.it>
2025-04-10 18:15:59 +0200

    Update ICurator.java

--- Marchet/api/main/java/unicam/marchet/ICurator.java
+++ Marchet/api/main/java/unicam/marchet/ICurator.java
@@ -5,5 +5,16 @@
 * @author Ilaria Morettini, Erika Aguiari, Luca Barchiesi
 */
 public interface ICurator implements IUser {
-   
+
+   /**
+   * This method is used to accept an information submitted by a vendor.
+   */
+   void accept();
+
+   /** 
+   * This method is used to decline an information submitted by a vendor
+   *
+   * @param motivation, the reason why the information was declined
+   */
+   void decline(String motivation);
 }
