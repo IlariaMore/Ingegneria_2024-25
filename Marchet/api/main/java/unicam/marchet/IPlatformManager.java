e7e581df25ca72a4b07668ff792f9ac402ef1a69
Luca Barchiesi <luca.barchiesi@studenti.unicam.it>
2025-04-10 17:37:53 +0200

    Update IPlatformManager.java

--- Marchet/api/main/java/unicam/marchet/IPlatformManager.java
+++ Marchet/api/main/java/unicam/marchet/IPlatformManager.java
@@ -1,9 +1,26 @@
+/**
+*
+* This interface must be implemented by the class who manages the platform
+*
+*/
+
 public interface IPlatformManager implements IUser{
     
+    /**
+    * This method allows the Platform Manager to create a new curator
+    */
     void createCurator(); 
 
+    /**
+    * This method allows the Platform Manager to ban an account from the marketplace
+    *
+    * @param user, the user that needs to be banned
+    */
     void ban(user User);
 
+    /**
+    * This method allows the Platform Manager to access the database
+    */
     void accessDatabase();
 
 }
