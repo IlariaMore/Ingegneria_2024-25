ec0db047e0fa87493c098c4387ef7af146b94a11
Luca Barchiesi <luca.barchiesi@studenti.unicam.it>
2025-04-10 17:50:58 +0200

    Update IUser.java

--- Marchet/api/main/java/unicam/marchet/IUser.java
+++ Marchet/api/main/java/unicam/marchet/IUser.java
@@ -1,4 +1,31 @@
+
+/**
+*
+* This interface is implemented by the generic user.
+*
+* @author Ilaria Morettini, Erika Aguiari , Luca Barchiesi.
+*/
 public interface IUser{
-  public void access();
+  
+  /**
+  * This method allows to create a new account.
+  *
+  * @param username, the username chosed by the user.
+  *
+  * @param user, the role chosed by the user.
+  *
+  * @param password, the password choesd by the user
+  */
+  void createAccount(String username, User user, String password);
+  
+  /**
+  * This method is used to identify an already existing account.
+  * 
+  * @param username, the username chosed by the user.
+  *
+  * @param password, the password choesd by the user.  
+  */
+  void access(String username, String password);
+  
 }
 
