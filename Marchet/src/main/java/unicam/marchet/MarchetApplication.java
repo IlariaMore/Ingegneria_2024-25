f51be5d9c6be7d89d1a1ca39dec99e8a090382c5
IlariaMore <ilaria.morettini@studenti.unicam.it>
2025-04-10 16:07:03 +0200

    Add files via upload
    empty project, mainly //TODO

---
+++ Marchet/src/main/java/unicam/marchet/MarchetApplication.java
@@ -0,0 +1,13 @@
+package unicam.marchet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarchetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarchetApplication.class, args);
+    }
+
+}
---
+++ Marchet/src/test/java/unicam/marchet/MarchetApplicationTests.java
@@ -0,0 +1,13 @@
+package unicam.marchet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarchetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
