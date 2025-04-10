d403ca86a51886244ed322448e4591bbee739ce0
Luca Barchiesi <luca.barchiesi@studenti.unicam.it>
2025-04-10 19:00:02 +0200

    Update IProduct.java

--- Marchet/api/main/java/unicam/marchet/IProduct.java
+++ Marchet/api/main/java/unicam/marchet/IProduct.java
@@ -1,6 +1,22 @@
+/**
+*
+* This interface is used to represent a product in the marketplace
+*
+* @author Ilaria Morettini, Erika Aguiari, Luca Barchiesi
+*/
 public interface IProduct {
-  void setLabel();
+
+  /** 
+  * This method is used to name the product.
+  * 
+  * @param label, the name of the product.
+  */
+  void setLabel(String label);
+
+  
   void getLabel(); 
+  void setPrice();
+  void getPrice();
   void applyInformation(String information);
   void modifyInformation();
   void addCertification(Certification certification);
