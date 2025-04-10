9636365928575dceea606d1823abf31bf14ad98f
IlariaMore <ilaria.morettini@studenti.unicam.it>
2025-04-10 18:03:50 +0200

    Update IEntertainer.java

--- Marchet/api/main/java/unicam/marchet/IEntertainer.java
+++ Marchet/api/main/java/unicam/marchet/IEntertainer.java
@@ -1,4 +1,11 @@
 public interface IEntertainer implements IUser{
-  void createEvent(String name, )
-  
+  /*
+  * it creates a new event using the following parameters.
+  * @PARAM name for the name of the event
+  * @PARAM place for where the event is taking place
+  * @PARAM maxAttendees for the supported attendance
+  * @PARAM date to say when the event is taking place
+  * @PARAM description to give a quick description of the event
+  */
+  void createEvent(String name,String place, int maxAttendees, DATE date, String description );
 }
