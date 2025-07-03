db9bc70498d1f5addec45cdbd32a2201181548a4
erik-agu <erika.aguiari@studenti.unicam.it>
2025-07-03 10:41:23 +0200

    Delete Marchet/api/main/java/unicam/marchet directory

--- Marchet/api/main/java/unicam/marchet/IEventManager.java
+++
@@ -1,3 +0,0 @@
-public interface IEventManager implements IEvent{
-  
-}
--- Marchet/api/main/java/unicam/marchet/Market.java
+++
@@ -1,9 +0,0 @@
-public interface Market {
-  private Database database;
-
-  // richiamato dall'acquirente 
-  public void showProducts();
-  // richiamati dal venditore
-  public void deleteproduct(Vendor v, Product p);
-  public void addProduct(Vendor v, Product p)
-}
