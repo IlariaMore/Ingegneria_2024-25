5abbd6f28d78c811671a6137229651badc8665dc
Luca Barchiesi <luca.barchiesi@studenti.unicam.it>
2025-07-09 10:00:01 +0200

    Add files via upload

---
+++ MarchetPlace/src/test/java/unicam/cs/ids/Marchet/MarchetApplicationTests.java
@@ -0,0 +1,13 @@
+package unicam.cs.ids.Marchet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarchetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
