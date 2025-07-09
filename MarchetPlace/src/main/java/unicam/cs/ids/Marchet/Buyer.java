fdc2a87be64c3ef91d54b92bc2041aecc60c79ae
Luca Barchiesi <luca.barchiesi@studenti.unicam.it>
2025-07-09 18:52:22 +0200

    Update Buyer.java

--- MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Buyer.java
+++ MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Buyer.java
@@ -1,4 +1,12 @@
+/**
+*
+* This method creates an object Product.
+*
+* @return an object Product.
+*
+*/
 public class Buyer {
+    
     private String name;
 
     private ShoppingCart shoppingCart;
@@ -9,6 +17,8 @@ public class Buyer {
 
     private static int currentId;
 
+
+    
     private static int getCurrentId() {
         return currentId;
     }
@@ -25,21 +35,76 @@ public class Buyer {
         return id;
     }
 
+    public Mailbox getMailbox(mailbox) {
+        return mailbox;
+    }
+
+    
+    /**
+    *
+    * This method adds a product to the shopping cart.
+    *
+    * @param product, the product that needs to be added.
+    *
+    * @return true if the product is added successfully, false otherwise.
+    *
+    */
     public boolean addToShoppingCart(Product product) {
         return false;
     }
 
+    
+    /**
+    *
+    * This method is used to buy all the products in the shopping cart.
+    *
+    * @return true if the products are sold successfully, false otherwise.
+    *
+    */
     public boolean buyShoppingCart(){
         return false;
     }
 
+    
+    /**
+    *
+    * This method is used to buy a ticket of an event.
+    *
+    * @param event, the event from which the user is buying the ticket.
+    *
+    * @return true if the event ticket is sold successfully, false otherwise.
+    *
+    */
     public boolean buyEventTicket(Event event) {
-
+        return false
     }
 
+    
+    /**
+    *
+    * This method is used to update the buyer about the event status.
+    *
+    * @param event, the event that is being observed.
+    *
+    */
     public void update(Event event) {}
 
-    public Mailbox getMailbox() {
-        return mailbox;
+    
+    /**
+    *
+    * This method is used to open the user mailbox;
+    *
+    */
+    public void openMailbox() {
+    }
+
+    
+    /**
+    *
+    * This method is used to refresh the mailbox.
+    *
+    */
+    public void refreshMailbox() {
+        mailbox.refresh();
     }
 }
