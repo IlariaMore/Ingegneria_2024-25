7346118ab9800b1b5a63d02f2a6ab9d8509f6b64
Luca Barchiesi <luca.barchiesi@studenti.unicam.it>
2025-09-14 21:39:31 +0200

    Update ProductBoard.java

--- MarchetPlace/src/main/java/unicam/cs/ids/Marchet/ProductBoard.java
+++ MarchetPlace/src/main/java/unicam/cs/ids/Marchet/ProductBoard.java
@@ -1,23 +1,91 @@
 import java.util.ArrayList;
 
+/**
+ * This class represents the container for the product to sell
+ *
+ * @author Erika Aguiari, Ilaria Morettini, Luca Barchiesi
+ */
 public class ProductBoard {
 
+    /**
+     * The list of products containing all the products
+     */
     private ArrayList<Product> products;
 
-    public boolean addProduct() {
+    /**
+     * Creates a new product board
+     */
+    public ProductBoard(){
+        this.products = new ArrayList<Product>();
+    }
+
+    /**
+     * Adds a new product to the product board
+     *
+     * @param product, the product to add
+     *
+     * @return true if it is inserted successfully, false otherwise
+     */
+    public boolean addProduct(Product product) {
+        return products.add(product);
+    }
+
+    /**
+     * Removes a product to the product board
+     *
+     * @param product, the product to remove
+     *
+     * @return true if it is removed successfully, false otherwise
+     */
+    public boolean removeProduct(Product product) {
+        return products.remove(product);
+    }
+
+    /**
+     * Removes a product to the product board
+     *
+     * @param id, the id of the product to remove
+     *
+     * @return true if it is removed successfully, false otherwise
+     */
+    public boolean removeProduct(int id) {
+        for( Product product :  products) {
+            if (product.getId() == id) {
+                products.remove(product);
+                return true;
+            }
+        }
         return false;
     }
 
-    public boolean removeProduct() {
-        return false;
-    }
-
+    /**
+     * Verifies whether a product is in the product board
+     *
+     * @param product, the product searched for
+     *
+     * @return true if the product is contained, false otherwise
+     */
     public boolean contains(Product product) {
-        return false;
+        return products.contains(product);
     }
 
-    public ArrayList<Event> getProducts(Seller seller) {
-        return null;
+    public ArrayList<Product> getProducts(){
+        return products;
+    }
+
+    /**
+     * Returns a list of all the products a specific seller has published
+     *
+     * @param seller, the seller from witch taking the products
+     *
+     * @return an ArrayList of products created by the seller given
+     */
+    public ArrayList<Product> getProducts(Seller seller) {
+        ArrayList<Product> sublist = new ArrayList<Product>();
+        for (Product product : products) {
+            if (product.getCreator().equals(seller)) sublist.add(product);
+        }
+        return sublist;
     }
 
 }
