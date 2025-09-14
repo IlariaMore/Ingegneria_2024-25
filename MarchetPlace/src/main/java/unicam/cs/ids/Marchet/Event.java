d7475db832934413acd5008d934f962e4737f901
Luca Barchiesi <luca.barchiesi@studenti.unicam.it>
2025-09-14 21:43:10 +0200

    Update Event.java

--- MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Event.java
+++ MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Event.java
@@ -1,4 +1,7 @@
+import unicam.cs.ids.Marchet.Visitor;
+import unicam.cs.ids.Marchet.Visitable;
 import java.util.ArrayList;
+
 
 /**
 *
@@ -7,66 +10,145 @@ import java.util.ArrayList;
 * @author Erika Aguiari, Ilaria Morettini, Luca Barchiesi
 *
 */
-public class Event implements Visitable{
+public class Event implements Visitable {
 
+    /**
+     * The name of the event
+     */
     private String name;
 
+    /**
+     * The creator of the event
+     */
+    private Animator creator;
+
+    /**
+     * The maximum number of attendees, if set to 0 there's no limit
+     */
     private int maxAttendees;
 
+    /**
+     * The description of the event
+     */
     private String description;
 
+    /**
+     * The id of the event
+     */
     private int id;
 
     private static int currentId;
 
+    /**
+     * The collaborators to the event
+     */
     private ArrayList<Seller> collaborators;
 
+    /**
+     * The people who have subscribes to join the event
+     */
     private ArrayList<Buyer> subscribers;
 
-    public Event(){}
+    /**
+     * Creates a new event
+     */
+    public Event(String name, String description, Animator creator, ArrayList<Seller> collaborators){
+        this.name=name;
+        this.description=description;
+        this.creator=creator;
+        this.collaborators=collaborators;
+        this.id=currentId++;
+    }
 
-    
+    /**
+     * Creates a new event with a set number of attendees
+     */
+    public Event(String name, String description, Animator creator, ArrayList<Seller> collaborators, int maxAttendees){
+        this.name=name;
+        this.description=description;
+        this.creator=creator;
+        this.collaborators=collaborators;
+        this.maxAttendees=maxAttendees;
+        this.id=currentId++;
+    }
+
     private static int getCurrentId() {
         return currentId;
     }
 
-    @Override
-    public void accept(Visitor visitor) {
-        visitor.visit(this);
-    }
-
+    /**
+     * @return the name of the event
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @param name of the event
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return the creator of the event
+     */
+    public Animator getCreator() {
+        return creator;
+    }
+
+    /**
+     * @param creator of the event
+     */
+    public void setCreator(Animator creator) {
+        this.creator = creator;
+    }
+
+    /**
+     * @return the maximum number of attendees
+     */
     public int getMaxAttendees() {
         return maxAttendees;
     }
 
+    /**
+     * @param maxAttendees, the maximum number of attendees
+     */
     public void setMaxAttendees(int maxAttendees) {
         this.maxAttendees = maxAttendees;
     }
 
+    /**
+     * @return the description of the event
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * @param description, the description of the event
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * @return the id of the event
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * @return the collaborators of the event
+     */
     public ArrayList<Seller> getCollaborators() {
         return collaborators;
     }
 
+    /**
+     * @param collaborators, the collaborators of the event
+     */
     public void setCollaborators(ArrayList<Seller> collaborators) {
         this.collaborators = collaborators;
     }
@@ -77,11 +159,18 @@ public class Event implements Visitable{
     *
     * @param buyer, the buyer who wants to subscribe to an event.
     *
-    * @return @true if the operation ended successfully, false otherwise.
+    * @return true if the operation ended successfully, false otherwise.
     *
     */
     public boolean subscribe(Buyer buyer) {
-        return false;
+        if(maxAttendees!=0) {
+            if(subscribers.size()==maxAttendees)
+                return false;
+        }
+        if (!subscribers.contains(buyer)){
+            subscribers.add(buyer);
+            return true;
+        } else return false;
     }
 
     /**
@@ -94,6 +183,41 @@ public class Event implements Visitable{
     *
     */
     public boolean unsubscribe(Buyer buyer) {
-        return false;
+        if (subscribers.contains(buyer)){
+            subscribers.remove(buyer);
+            return true;
+        } else return false;
     }
+
+    /**
+     * Notifies the subscribers when needed
+     *
+     * @param message, the message to spread
+     */
+    public void notifySubscribers(String message) {
+        for (Buyer sub : subscribers) {
+            sub.update(this, message);
+        }
+    }
+
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+
+    @Override
+    public boolean equals(Object obj){
+
+        if (obj == null){
+            return false;
+        }
+        if(!(obj instanceof Event event)){
+            return false;
+        }
+        else{
+            return this.id == event.id;
+        }
+    }
+
 }
