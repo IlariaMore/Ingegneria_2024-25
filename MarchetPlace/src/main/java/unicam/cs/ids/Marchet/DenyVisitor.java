ae1ce35a60f6ae5c9f452809e46857e3a04e74e2
erik-agu <erika.aguiari@studenti.unicam.it>
2025-09-15 21:45:28 +0200

    Update DenyVisitor.java

--- MarchetPlace/src/main/java/unicam/cs/ids/Marchet/DenyVisitor.java
+++ MarchetPlace/src/main/java/unicam/cs/ids/Marchet/DenyVisitor.java
@@ -1,14 +1,25 @@
+package unicam.cs.ids.Marchet;
+
+import unicam.IdSProject.Event;
+import unicam.IdSProject.Product;
+
 public class DenyVisitor implements Visitor {
 
-    private RequestHandler requestHandler;
+    private String message;
+
+    public DenyVisitor(String message) {
+        this.message=message;
+    }
 
     @Override
     public void visit(Product product) {
-
+        product.getCreator().getMailbox().addMessage("La tua richiesta per il prodotto "+
+                product.getName()+ " è stata negata. \nMotivo: "+message);
     }
 
     @Override
     public void visit(Event event) {
-
+        event.getCreator().getMailbox().addMessage("La tua richiesta per l'evento "+
+                event.getName()+ " è stata negata. \nMotivo: "+message);
     }
 }
