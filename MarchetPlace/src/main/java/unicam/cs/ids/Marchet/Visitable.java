e1cb9a081f5642f67d2590316f3263ca8dc67c6a
erik-agu <erika.aguiari@studenti.unicam.it>
2025-09-15 21:36:11 +0200

    Update Visitable.java

--- MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Visitable.java
+++ MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Visitable.java
@@ -1,4 +1,14 @@
+package unicam.cs.ids.Marchet;
+
+/**
+ * This interface is implemented by those need to be verified by the curator
+ */
 public interface Visitable {
 
+    /**
+     * Accepts the visitor and ridirects the call to the visitor
+     *
+     * @param visitor
+     */
     public void accept(Visitor visitor);
 }
