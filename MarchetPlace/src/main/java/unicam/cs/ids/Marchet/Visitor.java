2aa3b2ba5040ec3db3d5fd99a9ac83d9f5d80742
erik-agu <erika.aguiari@studenti.unicam.it>
2025-09-15 21:35:32 +0200

    Update Visitor.java
    Updated Visitor

--- MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Visitor.java
+++ MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Visitor.java
@@ -1,6 +1,24 @@
+package unicam.cs.ids.Marchet;
+
+import unicam.IdSProject.Event;
+import unicam.IdSProject.Product;
+
+/**
+ * This interface declares the methods to visit the concrete visitables (those who implement the Visitable interface)
+ */
 public interface Visitor {
 
+    /**
+     * Visits a product
+     *
+     * @param product
+     */
     public void visit(Product product);
 
+    /**
+     * Visits an event
+     *
+     * @param event
+     */
     public void visit(Event event);
 }
