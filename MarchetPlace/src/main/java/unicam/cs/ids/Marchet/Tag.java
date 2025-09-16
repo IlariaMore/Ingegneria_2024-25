b98828577c10a084c3970a23da25af80deb219af
IlariaMore <ilaria.morettini@studenti.unicam.it>
2025-09-16 11:47:15 +0200

    Update Tag.java

--- MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Tag.java
+++ MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Tag.java
@@ -1,4 +1,20 @@
+package unicam.cs.ids.Marchet;
+
+/**
+* Tipi di Venditore
+* @see #PRODUCER
+* @see #TRANSFORMER
+*
+* @author Aguiari Erika, Barchiesi Luca, Ilaria Morettini
+*/
 public enum Tag {
+    /**
+    * Produttore
+    */
     PRODUCER,
-    TRANSFORMER
+
+    /**
+     * Trasformatore
+     */
+    TRANSFORMER
 }
