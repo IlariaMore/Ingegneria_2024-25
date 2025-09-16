2c1272daddcd23e23fa03d4afe2ce520a468ea5c
IlariaMore <ilaria.morettini@studenti.unicam.it>
2025-09-16 11:41:47 +0200

    Update Location.java

--- MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Location.java
+++ MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Location.java
@@ -1,26 +1,69 @@
+package unicam.cs.ids.Marchet;
+
+/**
+ * This class represents the location that a user can add to its profile,
+ * expressed through coordinates
+ */
 public class Location {
+    
+/**
+* The latitude of the location
+*/
+private String latitude;
 
-    private float latitude;
+/**
+* The longitude of the location
+*/
+private String longitude;
 
-    private float longitude;
-
-    public float getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(float latitude) {
-        this.latitude = latitude;
-    }
-
-    public float getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(float longitude) {
-        this.longitude = longitude;
-    }
-
-    public String toString() {
-        return "";
-    }
+public Location(String latitude, String longitude){
+    this.latitude=latitude;
+    this.longitude=longitude;
 }
+
+/**
+* @return the latitude of the location
+*/
+public String getLatitude() {
+    return latitude;
+}
+    
+ /**
+ * @param latitude of the location
+ */
+public void setLatitude(String latitude) {
+    this.latitude = latitude;
+}
+
+/**
+* @return the longitude of the location
+*/
+public String getLongitude() {
+    return longitude;
+}
+
+/**
+* @param longitude of the location
+*/
+public void setLongitude(String longitude) {
+    this.longitude = longitude;
+}
+
+@Override
+public String toString() { return this.latitude+this.longitude; }
+
+@Override
+public boolean equals(Object obj){
+    if (obj == null){
+        return false;}
+    if (obj instanceof Location location) {
+        if (this.getLatitude().equals(location.getLatitude()) && this.getLongitude().equals(location.getLongitude())) {
+                return true;
+        }
+    }
+        return false;
+    }
+}
+
+
+
