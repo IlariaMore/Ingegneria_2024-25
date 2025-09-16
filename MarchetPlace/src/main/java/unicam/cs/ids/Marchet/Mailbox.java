da22e4abbf615dd3a25acf2e8118f1f353cdb751
IlariaMore <ilaria.morettini@studenti.unicam.it>
2025-09-16 11:44:27 +0200

    Update Mailbox.java

--- MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Mailbox.java
+++ MarchetPlace/src/main/java/unicam/cs/ids/Marchet/Mailbox.java
@@ -1,14 +1,46 @@
+package unicam.cs.ids.Marchet;
+
 import java.util.ArrayList;
 
+/**
+ * This class represents the mailbox where users receive messages
+ *
+ * @author Aguiari Erika, Ilaria Morettini, Barchiesi Luca
+ */
 public class Mailbox {
 
+    /**
+     * The mailbox where messages are stored
+     */
     private ArrayList<String> mailbox;
 
-    public void addMessage(String message) {
-        mailbox.add(message);
+    /**
+     * Creates a new Mailbox
+     */
+    public Mailbox(){
+        mailbox = new ArrayList<String>();
     }
 
-    public void refreshMailbox() {
-        mailbox.clear();
+    /**
+     * Adds a message to the mailbox
+     *
+     * @param message to add
+     */
+    public void addMessage(String message) {
+            mailbox.add(message);
     }
+
+    /**
+     * @return the messages inside the mailbox
+     */
+    public ArrayList<String> getMessages() {
+        return mailbox;
+    }
+
+    /**
+     * Deletes all the messages inside the mailbox
+     */
+    public void refresh() {
+        mailbox.clear();
+    }
 }
